bd32a48f70cb9c94d3eca45e5842307124a309a4
dominic.hueckmann <Dominic.Hueckmann@indeca.de>
2022-04-29 17:00:35 +0200

    Fix config

--- next.config.js
+++ next.config.js
@@ -1,20 +1,9 @@
 /** @type {import('next').NextConfig} */
-const path = require('path')
-const withSass = require('@zeit/next-sass');
-module.exports = withSass({
-	/* bydefault config  option Read For More Optios
-  here https://github.com/vercel/next-plugins/tree/master/packages/next-sass
-  */
-	cssModules: true,
-});
 const nextConfig = {
 	reactStrictMode: true,
 };
 
 module.exports = {
 	nextConfig,
-	sassOptions: {
-		includePaths: [path.join(__dirname, 'styles')],
-	},
 };
 
